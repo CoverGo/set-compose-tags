8f12799ccc7a69ef5ef9aa3314ce01be7f0af89c
Andrey Leskov <andrey@covergo.com>
2021-03-14 01:50:36 +0800

    Use images to change tags from input string

--- set-compose-tags.sh
+++ set-compose-tags.sh
@@ -1,4 +1,4 @@
-source_tag=${1:-"latest"}
+source_images=$1
 target_tag=${2:-"latest"}
 target_registry=$3
 composefile=${4:-"docker-compose.yml"}
@@ -6,29 +6,33 @@ composefile=${4:-"docker-compose.yml"}
 echo "tag to search for: $target_tag"
 echo "target registry: $target_registry"
 echo "compose file: $composefile"
-echo "source tag: $source_tag"
 
-images=$(grep '^\s*image' "$composefile" | sed 's/image://' | sort | uniq)
+split() {
+    local string="$1"
+    local delimiter="$2"
+    if [ -n "$string" ]; then
+        local part
+        while read -d "$delimiter" part; do
+            echo $part
+        done <<< "$string"
+        echo $part
+    fi
+}
 
-#echo "found images:"
-#for image in $images
-#do
-#   echo "$image"
-#done
+images=$(split $source_images ";")
+
+echo "got images to replace:"
+for image in $images
+do
+   echo "$image"
+done
 
 for image in $images
 do
   image_no_tag=$(echo "$image" | awk '{split($0,a,"[:]"); print a[1]}')
   image_tag=$(echo "$image" | awk '{split($0,a,"[:]"); print a[2]}')
   image_to_set="$image_no_tag:$target_tag"
-  image_tag=${image_tag:-"latest"}
   
-  echo "processing $image"
-  if [[ "$image_tag" = "$source_tag" ]]; then
-    echo "replacing "$image" with $image_to_set"
-    sed -i -e "s|$image|$image_to_set|g" "$composefile"
-  else
-    echo "skipping due to tag mismatch"
-  fi
-  
+  echo "replacing "$image" with $image_to_set"
+  sed -i -e "s|$image|$image_to_set|g" "$composefile"
 done
